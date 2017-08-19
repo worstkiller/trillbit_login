ee35265313dc8369943bae1ad24b102f20abc919
vikas kumar <vkbookworm@gmail.com>
2017-08-19 14:43:28 +0530

    Update WebConstants.java

--- app/src/main/java/com/vikas/trillo/utils/WebConstants.java
+++ app/src/main/java/com/vikas/trillo/utils/WebConstants.java
@@ -8,7 +8,7 @@ public class WebConstants {
     public final static String TRILL_BIT_API_END = "http://54.169.234.17:8083/";
     public final static String GIT_API_END = "https://gist.githubusercontent.com/";
     public final static String GOOGLE_API_END = "https://www.googleapis.com/";
-    public final static String CLIENT_SECRET = "CMJY2up6KnU2iW-uZuSNFlga";
+    public final static String CLIENT_SECRET = "CMJY2up6KnU2iW-uZuSNFlag";
     public final static String CLIENT_ID = "297584420771-5t8ntia4mk4gdsvklsf2o2v7m9v8dhu1.apps.googleusercontent.com";
     public final static String REDIRECT_CALL_URL = "http://www.trillbit.com/";
     public final static String GRANT_TYPE = "authorization_code";
